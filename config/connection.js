2118984f94de1f3abaeeebdd152385653ef97b56
smlong203 <s.m.long203@outlook.com>
2018-09-17 23:51:10 -0500

    files

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,14 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
-
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id: " + connection.threadId);
-});
-
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'burgers_db'
+  });
+};
+connection.connect();
 module.exports = connection;
